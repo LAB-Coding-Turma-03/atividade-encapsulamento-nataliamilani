659f8e7d0ada707f64d57d055dcc6fa98547f81a
Natalia Milani <natalia.milani@hotmail.com>
2021-05-29 11:12:41 -0300

    atividade encapsulamento

--- src/test/java/br/com/impacta/lab/JavaSpringTemplateApplicationTests.java
+++ src/test/java/br/com/impacta/lab/JavaSpringTemplateApplicationTests.java
@@ -2,19 +2,36 @@ package br.com.impacta.lab;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import br.com.impacta.models.*;
 import org.junit.Test;
 
-class JavaSpringTemplateApplicationTests {
+public class JavaSpringTemplateApplicationTests {
 
 	@Test
 	public void shouldHaveCarwithEncapsulatePrinciple() {
 		
+    /*
+		 * Transforme a tabela Carro em uma classe separada
+		 * no pacote br.com.impacta.models
+		 * 
+		 *  Criar a classe Carro agora utilizando o encapsulamento.
+		 *  Atributos privados e os metodos get e set para cada atributo.
+		 *  
+		 *  Tabela de Carro
+		 *  
+		 *  Cor				Ano							Valor do Carro
+		 * 	Amarelo			2010						10000.00
+ 		 * 
+		 * -----------------------------------------------------------------
+		 * 
+		 * 
+		 * 
+		 */
+
 		Carro carro = new Carro();
 		carro.setCor("Azul");
-		carro.setAno(2021);
-		carro.setValor(50000.00);
+		carro.setAno(2010);
+		carro.setValor(10000.00);
 		
 		assertEquals("Azul", carro.getCor());
 		
